9df6c3724d79158b452707ff61a14587c2ff3296
Marcos Del Sol Vives <marcos@dracon.es>
2017-12-05 09:41:00 +0100

    Added 2017 day 1 and 2

---
+++ 2017/day01_b.py
@@ -0,0 +1,30 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+R \\\ !2CE %H@ \\\
+
+R *@F ?@E:46 2 AC@8C6DD 32C E92E ;F>AD E@ d_T 4@>A=6E:@?] pAA2C6?E=J[ E96 5@@C :D?VE J6E D2E:D7:65[ 3FE :E 5:5 6>:E 2 DE2C 2D 6?4@FC286>6?E] %96 :?DECF4E:@?D 492?86i
+
+R }@H[ :?DE625 @7 4@?D:56C:?8 E96 ?6IE 5:8:E[ :E H2?ED J@F E@ 4@?D:56C E96 5:8:E 92=7H2J 2C@F?5 E96 4:C4F=2C =:DE] %92E :D[ :7 J@FC =:DE 4@?E2:?D `_ :E6>D[ @?=J :?4=F56 2 5:8:E :? J@FC DF> :7 E96 5:8:E `_^a l d DE6AD 7@CH2C5 >2E496D :E] u@CEF?2E6=J[ J@FC =:DE 92D 2? 6G6? ?F>36C @7 6=6>6?ED]
+
+R u@C 6I2>A=6i
+
+    R `a`a AC@5F46D ei E96 =:DE 4@?E2:?D c :E6>D[ 2?5 2== 7@FC 5:8:ED >2E49 E96 5:8:E a :E6>D 29625]
+    R `aa` AC@5F46D _[ 3642FD6 6G6CJ 4@>A2C:D@? :D 36EH66? 2 ` 2?5 2 a]
+    R `abcad AC@5F46D c[ 3642FD6 3@E9 aD >2E49 6249 @E96C[ 3FE ?@ @E96C 5:8:E 92D 2 >2E49]
+    R `ab`ab AC@5F46D `a]
+    R `a`b`c`d AC@5F46D c]
+
+R (92E :D E96 D@=FE:@? E@ J@FC ?6H 42AE492n
+
+:>A@CE DJD
+
+7@C =:?6 :? DJD]DE5:?i
+	=:?6 l =:?6]DEC:AWX
+
+	DF> l _
+	92=7 l =6?W=:?6X ^^ a
+	7@C A@D :? C2?86W_[ 92=7Xi
+		:7 =:?6,A@D. ll =:?6,92=7 Z A@D.i
+			DF> Zl a Y :?EW=:?6,A@D.X
+
+	AC:?EWDF>X
---
+++ 2017/day02_b.py
@@ -0,0 +1,40 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+R \\\ !2CE %H@ \\\
+
+R QvC62E H@C<j =@@<D =:<6 H6VC6 @? E96 C:89E EC24< 27E6C 2==] w6C6VD 2 DE2C 7@C J@FC 677@CE]Q w@H6G6C[ E96 AC@8C2> D66>D 2 =:EE=6 H@CC:65] r2? AC@8C2>D 36 H@CC:65n
+
+R Qq2D65 @? H92E H6VC6 D66:?8[ :E =@@<D =:<6 2== E96 &D6C H2?E65 :D D@>6 :?7@C>2E:@? 23@FE E96 6G6?=J 5:G:D:3=6 G2=F6D :? E96 DAC625D966E] &?7@CEF?2E6=J[ ?@?6 @7 FD 2C6 6BF:AA65 7@C E92E <:?5 @7 42=4F=2E:@? \ >@DE @7 FD DA64:2=:K6 :? 3:EH:D6 @A6C2E:@?D]Q
+
+R xE D@F?5D =:<6 E96 8@2= :D E@ 7:?5 E96 @?=J EH@ ?F>36CD :? 6249 C@H H96C6 @?6 6G6?=J 5:G:56D E96 @E96C \ E92E :D[ H96C6 E96 C6DF=E @7 E96 5:G:D:@? @A6C2E:@? :D 2 H9@=6 ?F>36C] %96J H@F=5 =:<6 J@F E@ 7:?5 E9@D6 ?F>36CD @? 6249 =:?6[ 5:G:56 E96>[ 2?5 255 FA 6249 =:?6VD C6DF=E]
+
+R u@C 6I2>A=6[ 8:G6? E96 7@==@H:?8 DAC625D966Ei
+
+R d h a g
+R h c f b
+R b g e d
+
+    R x? E96 7:CDE C@H[ E96 @?=J EH@ ?F>36CD E92E 6G6?=J 5:G:56 2C6 g 2?5 aj E96 C6DF=E @7 E9:D 5:G:D:@? :D c]
+    R x? E96 D64@?5 C@H[ E96 EH@ ?F>36CD 2C6 h 2?5 bj E96 C6DF=E :D b]
+    R x? E96 E9:C5 C@H[ E96 C6DF=E :D a]
+
+R x? E9:D 6I2>A=6[ E96 DF> @7 E96 C6DF=ED H@F=5 36 c Z b Z a l h]
+
+R (92E :D E96 DF> @7 6249 C@HVD C6DF=E :? J@FC AFKK=6 :?AFEn
+
+:>A@CE DJD
+
+4964<DF> l _
+
+7@C =:?6 :? DJD]DE5:?i 
+	=:?6 l ,:?EWIX 7@C I :? =:?6]DA=:EWX.
+
+	7@C A@D :? C2?86W=6?W=:?6XXi
+		7@C A@Da :? C2?86W=6?W=:?6XXi
+			:7 A@D ll A@Dai
+				4@?E:?F6
+
+			:7 =:?6,A@D. T =:?6,A@Da. ll _i
+				4964<DF> Zl =:?6,A@D. ^^ =:?6,A@Da.
+
+AC:?EW4964<DF>X
