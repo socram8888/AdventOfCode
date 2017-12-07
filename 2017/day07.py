bb140c12b276da1bc29facb8cfdae6a3e5d8c5ef
Marcos Del Sol Vives <marcos@dracon.es>
2017-12-07 14:06:02 +0100

    Add day 7 part A

---
+++ 2017/day07.py
@@ -0,0 +1,35 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+:>A@CE DJD
+:>A@CE C6
+
+A2C6?ED l 5:4EWX
+?@56:?7@ l 5:4EWX
+
+7@C :?7@ :? DJD]DE5:?i
+	>2E49 l C6]>2E49WCVW-HZX -WW-5ZX-XWni \m W]YXXV[ :?7@X
+	:7 ?@E >2E49i
+		4@?E:?F6
+
+	?2>6 l >2E49]8C@FAW`X
+	H6:89E l :?EW>2E49]8C@FAWaXX
+
+	49:=5C6? l >2E49]8C@FAWbX
+	:7 49:=5C6?i
+		49:=5C6? l 49:=5C6?]DEC:AWX]DA=:EWV[ VX
+		7@C 49:=5 :? 49:=5C6?i
+			A2C6?ED,49:=5. l ?2>6
+	6=D6i
+		49:=5C6? l ,.
+
+	?@56:?7@,?2>6. l L
+		V?2>6Vi ?2>6[
+		VH6:89EVi H6:89E[
+		V49:=5C6?Vi 49:=5C6?
+	N
+
+E@A?@56 l ?6IEW:E6CWA2C6?ED]G2=F6DWXXX
+H9:=6 E@A?@56 :? A2C6?EDi
+	E@A?@56 l A2C6?ED,E@A?@56.
+
+AC:?EWV%@A ?@56i V Z E@A?@56X
