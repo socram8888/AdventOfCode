b7311a71795dc5098cbe5f005bf074c60ca229fc
Marcos Del Sol Vives <marcos@dracon.es>
2017-12-09 00:04:03 +0100

    Add day 8

---
+++ 2017/day08.py
@@ -0,0 +1,40 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+:>A@CE DJD
+:>A@CE C6
+7C@> 4@==64E:@?D :>A@CE 5672F=E5:4E
+
+r~|!p#x$~}$ l L
+	VllVi =2>352 2[ 3i 2 ll 3[
+	VPlVi =2>352 2[ 3i 2 Pl 3[
+	VkV i =2>352 2[ 3i 2 k  3[
+	VmV i =2>352 2[ 3i 2 m  3[
+	VklVi =2>352 2[ 3i 2 kl 3[
+	VmlVi =2>352 2[ 3i 2 ml 3
+N
+
+>6>@CJ l 5672F=E5:4EW=2>352i _X
+>2I6G6C l _
+
+7@C =:?6 :? DJD]DE5:?i
+	>2E49 l C6]>2E49WCVW-HZX W:?4M564X W\n-5ZX :7 W-HZX WllMPlMkMmMklMmlX W\n-5ZXV[ =:?6X
+	:7 ?@E >2E49i
+		AC:?EWV}@?>2E49:?8i V Z =:?6X
+		4@?E:?F6
+
+	:7C68 l >2E49]8C@FAWcX
+	:7@A l r~|!p#x$~}$,>2E49]8C@FAWdX.
+	:7G2= l :?EW>2E49]8C@FAWeXX
+
+	:7 :7@AW>6>@CJ,:7C68.[ :7G2=Xi
+		=:?6C68 l >2E49]8C@FAW`X
+		=:?6G2= l :?EW>2E49]8C@FAWbXX
+		:7 >2E49]8C@FAWaX ll V564Vi
+			=:?6G2= l \=:?6G2=
+
+		>6>@CJ,=:?6C68. Zl =:?6G2=
+		>2I6G6C l >2IW>2I6G6C[ >6>@CJ,=:?6C68.X
+
+>2I:E6> l >2IW>6>@CJ]:E6>DWX[ <6Jl=2>352 Ii I,`.X
+AC:?EWV|2I G2=F6i V Z C6ACW>2I:E6>XX
+AC:?EWV|2I 6G6Ci T:V T >2I6G6CX
