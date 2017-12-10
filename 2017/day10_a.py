c649577be5ab45454992c1c303a74001ea0c54e5
Marcos Del Sol Vives <marcos@dracon.es>
2017-12-10 14:02:44 +0100

    Added 2017 day 10

---
+++ 2017/day10_a.py
@@ -0,0 +1,28 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+:>A@CE DJD
+
+wp$w0{t} l ade
+
+7@C =:?6 :? DJD]DE5:?i
+	=6?8E9D l ,:?EWIX 7@C I :? =:?6]DEC:AWX]DA=:EWQ[QX.
+	
+	<?@ED l =:DEWC2?86Wwp$w0{t}XX
+	D<:A l _
+	@77D6E l _
+	7@C =6?8E9 :? =6?8E9Di
+		AC:?EWV{6?i T5V T =6?8E9X
+		7@C : :? C2?86W=6?8E9 ^^ aXi
+			A@D02 l W@77D6E Z :X T wp$w0{t}
+			A@D03 l W@77D6E Z =6?8E9 \ : \ `X T wp$w0{t}
+			AC:?EWV$H2A T5 k\m T5V T WA@D02[ A@D03XX
+
+			R $H2A E96>
+			<?@ED,A@D02.[ <?@ED,A@D03. l <?@ED,A@D03.[ <?@ED,A@D02.
+
+		@77D6E l W@77D6E Z =6?8E9 Z D<:AX T wp$w0{t}
+		D<:A l WD<:A Z `X T wp$w0{t}
+
+		AC:?EWV$E2EFDi TDV T C6ACW<?@EDXX
+
+	AC:?EWVu:?2=i TDV T C6ACW<?@EDXX
