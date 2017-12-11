395d553b96be8540b8f9978a4674e7effc943581
Marcos Del Sol Vives <marcos@dracon.es>
2017-12-11 19:13:42 +0100

    Added 2017 day 11

---
+++ 2017/day11.py
@@ -0,0 +1,36 @@
+RP^FDC^3:?^6?G AJE9@?b
+
+:>A@CE DJD
+
+7@C =:?6 :? DJD]DE5:?i
+	>@G6D l =:?6]DEC:AWX]DA=:EWV[VX
+	I l J l _
+	>@DE072C l _
+
+	7@C >@G6 :? >@G6Di
+		:7 >@G6 ll V?Vi
+			J Zl `
+		6=:7 >@G6 ll VDVi
+			J \l `
+		6=D6i
+			@77D6E l I U `
+			:7 >@G6,_. ll V?Vi
+				J Zl @77D6E
+			6=:7 >@G6,_. ll VDVi
+				J \l ` \ @77D6E
+			6=D6i
+				C2:D6 tI46AE:@?WVx?G2=:5 5:C64E:@?i TDV T C6ACW>@G6XX
+
+			:7 >@G6,`. ll VHVi
+				I \l `
+			6=:7 >@G6,`. ll V6Vi
+				I Zl `
+			6=D6i
+				C2:D6 tI46AE:@?WVx?G2=:5 5:C64E:@?i TDV T C6ACW>@G6XX
+
+		DE6AD l I Z >2IWJ \ I ^^ a[ _X
+		>@DE072C l >2IWDE6AD[ >@DE072CX
+
+	AC:?EWVu:?2= A@D:E:@? )i T5[ *i T5V T WI[ JXX
+	AC:?EWV$E6AD C6BF:C65i T5V T DE6ADX
+	AC:?EWV|@DE 72Ci T5V T >@DE072CX
